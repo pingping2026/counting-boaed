d13b8b24d958b75faf3315cd838e8c0f39670653
pingping2026 <saulgoodman2022@163.com>
2026-03-15 00:57:08 +0800

    counting board

---
+++ index.js
@@ -0,0 +1,33 @@
+let result1 = 0
+document.getElementById("num1").textContent =result1
+let result2 = 0
+document.getElementById("num2").textContent =result2
+function addhome1(){
+    result1 +=1
+    document.getElementById("num1").textContent =result1
+}
+1
+function addhome2(){
+    result1 +=2
+    document.getElementById("num1").textContent =result1
+}
+
+function addhome3(){
+    result1 +=3
+    document.getElementById("num1").textContent =result1
+}
+
+function addguest1(){
+    result2 +=1
+    document.getElementById("num2").textContent =result2
+}
+
+function addguest2(){
+    result2 +=2
+    document.getElementById("num2").textContent =result2
+}
+
+function addguest3(){
+    result2 +=3
+    document.getElementById("num2").textContent =result2
+}
